56dcb59fa5559836ec3c546d06c63363bd473887
seonghann <dmdtka0084@gmail.com>
2023-02-09 19:01:18 +0900

    __init__.py add

--- models/epsnet/__init__.py
+++ models/epsnet/__init__.py
@@ -4,29 +4,28 @@ def get_model(config):
 
         return DualEncoderEpsNetwork(config)
 
+    elif config.network == "dualenc_general":
+        from .dualenc_dimepp_newedge_nolocal import DualEncoderEpsNetwork
+        return DualEncoderEpsNetwork(config)
+
     elif config.network == "dualenc_sch_come":
         from .dualenc_sch_come import DualEncoderEpsNetwork
-
         return DualEncoderEpsNetwork(config)
 
     elif config.network == "dualenc_ver3_base_global":
         from .dualenc_ver3_base_global import DualEncoderEpsNetwork
-
         return DualEncoderEpsNetwork(config)
 
     elif config.network == "dualenc_ver3_base_nolocal":
         from .dualenc_ver3_base_nolocal import DualEncoderEpsNetwork
-
         return DualEncoderEpsNetwork(config)
 
     elif config.network == "dualenc_ver3_newedge_global":
         from .dualenc_ver3_newedge_global import DualEncoderEpsNetwork
-
         return DualEncoderEpsNetwork(config)
 
     elif config.network == "dualenc_ver3_newedge_nolocal":
         from .dualenc_ver3_newedge_nolocal import DualEncoderEpsNetwork
-
         return DualEncoderEpsNetwork(config)
 
     elif config.network == "mixed2denc_ver1":
